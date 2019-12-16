3275f741381cf7e125ffa3d8620742728f988878
Brad Krajina <bradkrajina@gmail.com>
2019-12-15 18:08:08 -0800

    organizing visualization and structure analysis scripts into new directories

--- simrun.py
+++ simrun.py
@@ -1,8 +1,10 @@
-#Python script for cleaning up data folders and generating input files
+# Python script for cleaning up data folders, generating input files, and
+# running a simulation
 
 import csv
 import numpy as np
 import os
+
 """Function for writing input file
 Inputs:
 outfile - file where parameter file is to be exported
@@ -12,54 +14,50 @@ templatefile - file that will be used as a template
 params - dictionary of param name strings and associated values
          (use consistent caps to be consistent with template)
 """
-def write_input(outfile,templatefile, params):
 
-    #Define space delimited dialect
-    csv.register_dialect('mydialect', delimiter = ' ')
-    #Define space delimited dialect
-    
+
+def write_input(outfile, templatefile, params):
+
+    # Define space delimited dialect
+    csv.register_dialect('mydialect', delimiter=' ')
+    # Define space delimited dialect
     param_dict = {}
-    
-    #Open the template file and read in as a dictionary of 
-    #paramater name, value pairs
-    with open(templatefile,'r') as template:
-        reader = csv.reader(filter(lambda row: row[0]!='#', template), dialect = 'mydialect')
+    # Open the template file and read in as a dictionary of
+    # paramater name, value pairs
+    with open(templatefile, 'r') as template:
+        reader = csv.reader(filter(lambda row: row[0] != '#', template),
+                            dialect='mydialect')
 
-        #Loop over paramname,value pairs in template
-        for param,value in reader:
+        # Loop over paramname,value pairs in template
+        for param, value in reader:
             param_dict[param] = value
-        
-    #Update param_dict with the new params
+    # Update param_dict with the new params
     for param in params:
         param_dict[param] = params[param]
-            
-    #Open the target output file for writing
+    # Open the target output file for writing
     with open(outfile, 'w') as inputfile:
-        writer = csv.writer(inputfile,dialect = 'mydialect')
-        #Loop over the param dictionary and save as key,value pairs
+        writer = csv.writer(inputfile, dialect='mydialect')
+        # Loop over the param dictionary and save as key,value pairs
         for param in param_dict:
-            writer.writerow([param,param_dict[param]])
+            writer.writerow([param, param_dict[param]])
 
 
-#################################################
-#Set up input files
-#################################################            
+# ################################################
+# Set up input files and data folders
+# ################################################
 
 
-#Set the LKs for the simulation
-LKs = [0,1,2,3]
-np.savetxt('input/LKs',LKs, fmt = '%i')
+# Set the LKs for the simulation
+LKs = [0, 1, 2, 3]
+np.savetxt('input/LKs', LKs, fmt='%i')
 
 
-#Clean up data folder
+# Clean up data folder
 os.system('mv data/* trash/')
 
-#Generate data folders for each LK
+# Generate data folders for each LK
 for lk in LKs:
-    os.system('mkdir data/LK_%s' %lk)
-
-#compile the simulation
-os.system('./compile.sh')
+    os.system('mkdir data/LK_%s' % lk)
 
 NINIT = 100
 L = 1000.
@@ -69,10 +67,18 @@ NREPLICAEXCHANGE = 100
 INDMAX = 100
 PTON = 'T'
 LK = 0
-params = {'NINIT':NINIT, 'L': L,'INDMAX':INDMAX,'NSTEP':NSTEP, 'PTON':PTON, 'LK':LK, 'NREPLICAEXCHANGE': NREPLICAEXCHANGE, 'NB' : NB}
+params = {'NINIT': NINIT, 'L': L, 'INDMAX': INDMAX, 'NSTEP': NSTEP,
+          'PTON': PTON, 'LK': LK, 'NREPLICAEXCHANGE': NREPLICAEXCHANGE,
+          'NB': NB}
 
-write_input('input/input','input/template',params)
+write_input('input/input', 'input/template', params)
 
+# #################################################
+# Compile and run the simulation
+# #################################################
 
-#run the simulation with the appropriate number of processors
-os.system('mpirun -np %i ./wlcsim' %(len(LKs)+1))
+# compile the simulation
+os.system('./compile.sh')
+
+# run the simulation with the appropriate number of processors
+os.system('mpirun -np %i ./wlcsim' % (len(LKs)+1))
